8f95cc3eca4770e90c6de306937027a91130304c
Kanhaiya Thakur <kanhaiya.thakur@in.g4s.com>
2018-02-21 10:22:29 +0000

    Adding comment

--- src/main/java/com/ocean/springboot/data/mongo/repository/UserRepository.java
+++ src/main/java/com/ocean/springboot/data/mongo/repository/UserRepository.java
@@ -24,6 +24,6 @@ public interface UserRepository extends MongoRepository<User, String>
 {
 	User findByUsername(String username);
 	
-	@Query(value="{'userDetail.address': {$regex: ?0}}")
+	@Query(value="{'userDetail.address': {$regex: ?0}}")		// This is equals to: address like '%text%'
 	List<User> findByAddress(String address);
 }
